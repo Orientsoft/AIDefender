5b831f855e48d3b72e3a4cfb043d04e6c7fcad9a
gaorong <gaorong@orientsoft.cn>
2018-01-30 11:24:25 +0800

    fix bug

--- src/routes/systemquery/kpi.js
+++ src/routes/systemquery/kpi.js
@@ -8,7 +8,7 @@ export default class Index extends React.Component {
     return (
       <div>
         <div />
-        <KPIChart data={data} />
+        <KPIChart chartConfigs={data.chartConfigs} dataSource={data.dataSource} />
       </div>
     )
   }

e6480c66f37940b504f74714073740c78962fd38
Robot Jiang <hellolaojiang@gmail.com>
2018-02-02 10:03:13 +0800

    modify mapnode

--- src/services/consts.js
+++ src/services/consts.js
@@ -1,3 +1,3 @@
-export const DS_CONFIG = 'ds-config'
-export const KPI_CONFIG = 'kpi-config'
+export const DS_CONFIG    = 'ds-config'
+export const KPI_CONFIG   = 'kpi-config'
 export const ALERT_CONFIG = 'alert-config'

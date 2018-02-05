10cae01f30b23ddb4461ac28fed2cf83a7075b4e
pixcai <pixcai@163.com>
2018-02-05 17:16:59 +0800

    Update add data source

--- src/utils/fields.js
+++ src/utils/fields.js
@@ -1,26 +1,37 @@
-import get from 'lodash/get';
-import forEach from 'lodash/forEach';
-import compact from 'lodash/compact';
-import flattenDeep from 'lodash/flattenDeep';
-import isPlainObject from 'lodash/isPlainObject';
+import get from 'lodash/get'
+import forEach from 'lodash/forEach'
+import compact from 'lodash/compact'
+import flattenDeep from 'lodash/flattenDeep'
+import isPlainObject from 'lodash/isPlainObject'
+import uniq from 'lodash/uniq'
 
 /**
  * 获取返回索引中包含的所有字段
  * @param {Object} mappings - 字段映射
  * @param {String} path - 字段层级
  */
-function fields(mappings, path = '') {
-    const result = [];
-    const properties = get(mappings, 'properties');
+function fields (mappings, path = '') {
+  const result = []
+  const properties = get(mappings, 'properties')
 
-    if (isPlainObject(properties)) {
-        forEach(properties, (mapping, field) => {
-            result.push(fields(mapping, compact([path, field]).join('.')));
-        });
-        return result;
-    }
-    
-    return path;
+  if (isPlainObject(properties)) {
+    forEach(properties, (mapping, field) => {
+      result.push(fields(mapping, compact([path, field]).join('.')))
+    })
+    return result
+  }
+
+  return path
 }
 
-export default mappings => flattenDeep(fields(mappings));
+export default (allMappings) => {
+  const result = []
+
+  forEach(allMappings, (mappingsValue) => {
+    forEach(mappingsValue.mappings, (mappings) => {
+      result.push(flattenDeep(fields(mappings)))
+    })
+  })
+
+  return uniq(flattenDeep(result))
+}

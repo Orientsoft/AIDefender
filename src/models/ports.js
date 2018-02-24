e1d1d207f2226e94ef8aa627d83899249c10ad57
jiangjun <925736803@qq.com>
2018-02-24 14:37:37 +0800

    update ports

--- src/models/ports.js
+++ src/models/ports.js
@@ -1,76 +1,93 @@
 import { getAllSource, addSource, getChoosedSource, deleteSource, updateSource } from 'services/ports'
+import moment from 'moment'
 
 export default {
-    namespace: 'ports',
+  namespace: 'ports',
 
-    state: {
-        ports: [],
-        choosedPort: {}
-    },
+  state: {
+    ports: [],
+    choosedPort: {},
+    pagination: {},
+  },
 
-    reducers: {
-        // 获取所有数据
-        getAllPorts(state, { payload }) {
-            return { ...state, ports: payload }
-        }, 
-        // 添加数据
-        addAllPort(state, { payload }) {
-            const ports = state.ports.concat(payload)
-            return { ...state, ports }
-        },
-        // 删除数据
-        deletePort(state, { payload }) {
-            let index = -1
-            state.ports.forEach((src, i) => {
-                if (src.id === payload) {
-                    index = i
-                }
-            })
-            if (index > -1) {
-                state.ports.splice(index, 1)
-            }
-            return { ...state }
-        },
-        // 更新指定数据
-        updatePort (state, { payload }) {
-            for (let key in state.ports) {
-                if (state.ports[key].id === payload.id) {
-                    state.ports[key] = payload
-                }
-            }
-            return { ...state }
-        },
-        // 获取指定数据
-        getChoosedPort(state, { payload }) {
-            return { ...state, choosedPort: payload }
-        },
-    },
+  reducers: {
+    // 获取所有数据
+    getAllPorts(state, { payload }) {
+      let ports = payload.ports
+      ports.forEach(item => {
+        const { createAt, updatedAt } = item
+        item.createdAt = moment(createAt).format('YYYY-MM-DD HH:mm:ss')
+        item.updatedAt = moment(updatedAt).format('YYYY-MM-DD HH:mm:ss')
+      })
 
-    effects: {
-        // 查询所有数据
-        * queryPorts({ payload }, { call, put }) {
-            const response = yield call(getAllSource, payload)
-            yield put({ type: 'getAllPorts', payload: response.data })
-        },
-        // 添加数据
-        * addPort({ payload }, { call, put }) {
-            let response = yield call(addSource, payload)
-            yield put({ type: 'addAllPort', payload: response.data })
-        },
-        // 获取指定数据
-        * queryChoosedSource({ payload }, { call, put }) {
-            const response = yield call(getChoosedSource, payload.id)
-            yield put({ type: 'getChoosedPort', payload: response.data })
-        },
-        // 删除指定数据
-        * delChoosedSource({ payload }, { call, put }) {
-            let response = yield call(deleteSource, payload.id)
-            yield put({ type: 'deletePort', payload: payload.id })
-        },
-        // 更新指定数据
-        * updateChoosedSource({ payload }, { call, put }) {
-            let response = yield call(updateSource, payload)
-            yield put({ type: 'updatePort', payload: response.data })
-        },
+      console.log(ports)
+      let pagination = payload._metadata
+      return { ...state, ports, pagination }
     },
+    // 添加数据
+    addAllPort(state, { payload }) {
+      const { createAt, updatedAt } = payload
+      payload.createdAt = moment(createAt).format('YYYY-MM-DD HH:mm:ss')
+      payload.updatedAt = moment(updatedAt).format('YYYY-MM-DD HH:mm:ss')
+      const ports = state.ports.concat(payload)
+      return { ...state, ports }
+    },
+    // 删除数据
+    deletePort(state, { payload }) {
+      let index = -1
+      state.ports.forEach((src, i) => {
+        if (src._id === payload) {
+          index = i
+        }
+      })
+      if (index > -1) {
+        state.ports.splice(index, 1)
+      }
+      return { ...state }
+    },
+    // 更新指定数据
+    updatePort(state, { payload }) {
+      const { createAt, updatedAt } = payload
+      payload.createdAt = moment(createAt).format('YYYY-MM-DD HH:mm:ss')
+      payload.updatedAt = moment(updatedAt).format('YYYY-MM-DD HH:mm:ss')
+      for (let key in state.ports) {
+        if (state.ports[key]._id === payload._id) {
+          state.ports[key] = payload
+        }
+      }
+      return { ...state }
+    },
+    // 获取指定数据
+    getChoosedPort(state, { payload }) {
+      return { ...state, choosedPort: payload }
+    },
+  },
+
+  effects: {
+    // 查询所有数据
+    * queryPorts({ payload }, { call, put }) {
+      const response = yield call(getAllSource, payload)
+      yield put({ type: 'getAllPorts', payload: response.data })
+    },
+    // 添加数据
+    * addPort({ payload }, { call, put }) {
+      let response = yield call(addSource, payload)
+      yield put({ type: 'addAllPort', payload: response.data })
+    },
+    // 获取指定数据
+    * queryChoosedSource({ payload }, { call, put }) {
+      const response = yield call(getChoosedSource, payload.id)
+      yield put({ type: 'getChoosedPort', payload: response.data })
+    },
+    // 删除指定数据
+    * delChoosedSource({ payload }, { call, put }) {
+      yield call(deleteSource, payload.id)
+      yield put({ type: 'deletePort', payload: payload.id })
+    },
+    // 更新指定数据
+    * updateChoosedSource({ payload }, { call, put }) {
+      let response = yield call(updateSource, payload)
+      yield put({ type: 'updatePort', payload: response.data })
+    },
+  },
 }

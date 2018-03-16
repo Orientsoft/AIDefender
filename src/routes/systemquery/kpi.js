6d299ae817f45b6260b6e64a7af99ced8f360686
pixcai <pixcai@163.com>
2018-03-16 17:34:49 +0800

    Use plotly.js for draw bar chart

--- src/routes/systemquery/kpi.js
+++ src/routes/systemquery/kpi.js
@@ -2,6 +2,41 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import isEqual from 'lodash/isEqual'
 import { KPIChart } from 'components'
+import Plotly from 'plotly.js'
+
+let trace1 = {
+  x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  y: [20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17],
+  type: 'bar',
+  name: 'Primary Product',
+  marker: {
+    color: 'rgb(49,130,189)',
+    opacity: 0.7,
+  },
+}
+
+let trace2 = {
+  x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  y: [19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16],
+  type: 'bar',
+  name: 'Secondary Product',
+  marker: {
+    color: 'rgb(204,204,204)',
+    opacity: 0.5,
+  },
+}
+
+let data = [trace1, trace2]
+
+let layout = {
+  title: '2013 Sales Report',
+  xaxis: {
+    tickangle: -45,
+  },
+  barmode: 'group',
+}
+
+// Plotly.newPlot('myDiv', data, layout);
 
 export default class Index extends React.Component {
   componentWillMount () {
@@ -17,6 +52,12 @@ export default class Index extends React.Component {
     if (!isKpiConfigSame || !isTimeRangeSame) {
       this.lastTimeRange = nextProps.app.globalTimeRange.map(t => t.clone())
       this.query(nextProps.config.kpiConfig)
+    }
+  }
+
+  initChart (el) {
+    if (el) {
+      Plotly.newPlot(el, data, layout)
     }
   }
 
@@ -43,6 +84,7 @@ export default class Index extends React.Component {
 
     return (
       <div>
+        <div ref={el => this.initChart(el)} />
         {Object.keys(kpiResult).map((mid, key) => (
           <KPIChart key={key} chartConfig={kpiConfig.find(c => c._id === mid)} dataSource={kpiResult[mid]} />
         ))}

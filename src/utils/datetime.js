3183d29d34b723070df12ff92fc991015bce4521
pixcai <pixcai@163.com>
2018-03-26 11:06:07 +0800

    Update .flowconfig and utils/datetime

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -1,39 +1,32 @@
 // @flow
 import moment from 'moment'
+import isFunction from 'lodash/isFunction'
+
+const formatLiterals: Array<string> = [
+  'year', 'month', 'week', 'day', 'hour', 'minute', 'second', 'millisecond',
+]
 
 /** 计算日期时间粒度
  * @params {Moment|Number|Date|String} startTs
  * @params {Moment|Number|Date|String} endTs
  * @return 返回一个表示粒度的字符串，和Elasticsearch日期粒度相同
  */
-export function computeGranulation (startTs: mixed, endTs: mixed): string {
-  startTs = moment(startTs)
-  endTs = moment(endTs)
+export function getGranulation (
+  startTs: mixed,
+  endTs: mixed,
+): Array<string> {
+  const granulation: Array<string> = []
+  const _startTs: moment.Moment = moment(startTs)
+  const _endTs: moment.Moment = moment(endTs)
 
-  if (startTs.year() !== endTs.year()) {
-    return 'year'
-  }
-  if (startTs.month() !== endTs.month()) {
-    return 'month'
-  }
-  if (startTs.week() !== endTs.week()) {
-    return 'week'
-  }
-  if (startTs.day() !== endTs.day()) {
-    return 'day'
-  }
-  if (startTs.hour() !== endTs.hour()) {
-    return 'hour'
-  }
-  if (startTs.minute() !== endTs.minute()) {
-    return 'minute'
-  }
-  if (startTs.second() !== endTs.second()) {
-    return 'second'
-  }
-  if (startTs.millisecond() !== endTs.millisecond()) {
-    return 'millisecond'
-  }
+  formatLiterals.forEach((ts) => {
+    const startTsFn = _startTs[ts]
+    const endTsFn = _endTs[ts]
+    if (isFunction(startTsFn) && isFunction(endTsFn) &&
+      startTsFn() !== endTsFn()) {
+      granulation.push(ts)
+    }
+  })
 
-  return 'day'
+  return granulation
 }

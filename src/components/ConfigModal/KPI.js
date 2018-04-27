ade055d4fdcd627814768965a7b97fdd81705d6d
pixcai <pixcai@163.com>
2018-04-27 17:34:40 +0800

    Sortable kpi and alerts in config node

--- src/components/ConfigModal/KPI.js
+++ src/components/ConfigModal/KPI.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import noop from 'lodash/noop'
-import { Select, Collapse } from 'antd'
+import { Select, Collapse, Checkbox, Icon } from 'antd'
+import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc'
+import styles from './ConfigModal.less'
 
 const { Option } = Select
 const { Panel } = Collapse
@@ -17,6 +19,7 @@ export default class KPI extends React.Component {
     super(props)
     this.state = {
       data: props.value,
+      sortable: false,
     }
   }
 
@@ -29,15 +32,38 @@ export default class KPI extends React.Component {
     onChange(ids)
   }
 
-  onSortEnd = ({ oldIndex, newIndex }) => {
+  onSortableChange = (e) => {
     this.setState({
-      data: arrayMove(this.state.data, oldIndex, newIndex),
+      sortable: e.target.checked,
     })
   }
 
+  onSortEnd = ({ oldIndex, newIndex }) => {
+    const { onChange = noop } = this.props
+
+    this.setState({
+      data: arrayMove(this.state.data, oldIndex, newIndex),
+    }, () => onChange(this.state.data))
+  }
+
   render () {
-    const { data = [] } = this.state
+    const { data = [], sortable } = this.state
     const { defaultValue = [] } = this.props
+    const SortableItem = SortableElement(({ value }) => {
+      return <li className={styles.item}><Icon type="retweet" />{value}</li>
+    })
+    const SortableList = SortableContainer(({ style, items }) => {
+      return (
+        <ul style={style} className={styles.itemContainer}>
+          {items.map((item, index) => {
+            const dv = defaultValue.find(v => v._id === item)
+            return dv && (
+              <SortableItem key={dv._id} index={index} value={`${dv.name} (${dv._id})`} />
+            )
+          })}
+        </ul>
+      )
+    })
 
     return (
       <div>
@@ -52,16 +78,25 @@ export default class KPI extends React.Component {
             <Option key={key} value={dv._id}>{dv.name}</Option>
           ))}
         </Select>
-        <Collapse style={{ marginTop: '1em' }} bordered={false}>
-          {data.map((id) => {
-            const dv = defaultValue.find(v => v._id === id)
-            return dv && (
-              <Panel header={`${dv.name} (${dv._id})`} key={dv._id}>
-                {JSON.stringify(dv)}
-              </Panel>
-            )
-          })}
-        </Collapse>
+        {data.length > 1 && (
+          <div style={{ marginTop: '1em' }}>
+            <Checkbox checked={sortable} onChange={this.onSortableChange}>启用排序(拖动下方文字调整图表展示顺序)</Checkbox>
+          </div>
+        )}
+        {sortable ? (
+          <SortableList style={{ marginTop: '1em' }} items={data} onSortEnd={this.onSortEnd} />
+        ) : (
+          <Collapse style={{ marginTop: '1em' }} bordered={false} className="sortable-list">
+            {data.map((id) => {
+              const dv = defaultValue.find(v => v._id === id)
+              return dv && (
+                <Panel header={`${dv.name} (${dv._id})`} key={dv._id}>
+                  {JSON.stringify(dv)}
+                </Panel>
+              )
+            })}
+          </Collapse>
+        )}
       </div>
     )
   }

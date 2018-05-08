d2bf94d20793094b4b1ea4f0559dc1fb8bf046d8
jiangjun <925736803@qq.com>
2018-05-08 14:34:04 +0800

    update login

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,68 +1,138 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'dva'
-import { Button, Row, Form, Input } from 'antd'
+import { Button, Row, Form, Input, Icon } from 'antd'
 import config from '../../../app.json'
 import styles from './index.less'
 
 const FormItem = Form.Item
 
-const Login = ({
-  loading,
-  dispatch,
-  form: {
-    getFieldDecorator,
-    validateFieldsAndScroll,
-  },
-}) => {
-  function handleOk () {
-    validateFieldsAndScroll((errors, values) => {
+class Index extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleOk() {
+    this.props.form.validateFieldsAndScroll((errors, values) => {
       if (errors) {
         return
       }
-      dispatch({ type: 'login/login', payload: values })
+      this.props.dispatch({ type: 'login/login', payload: values })
     })
   }
 
-  return (
-    <div className={styles.form}>
-      <div className={styles.logo}>
-        <img alt="logo" src={config.logo} />
-        <span>{config.name}</span>
+  render() {
+    const {
+      getFieldDecorator,
+      validateFieldsAndScroll,
+    } = this.props.form
+    return (
+      <div className={styles.login}>
+        <div className={styles.form}>
+          <div className={styles.title}>
+            {/* <img alt="logo" src={config.logo} /> */}
+            <span>登录</span>
+          </div>
+          <form>
+            <FormItem hasFeedback>
+              {getFieldDecorator('username', {
+                rules: [
+                  {
+                    required: true,
+                  },
+                ],
+              })(<Input autoComplete="username" onPressEnter={() => this.handleOk()} placeholder="用户名" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} />)}
+            </FormItem>
+            <FormItem hasFeedback>
+              {getFieldDecorator('password', {
+                rules: [
+                  {
+                    required: true,
+                  },
+                ],
+              })(<Input type="password" autoComplete="current-password" onPressEnter={() => this.handleOk()} placeholder="密码" prefix={<Icon type="key" style={{ color: 'rgba(0,0,0,.25)' }} />} />)}
+            </FormItem>
+            <Row>
+              <Button type="primary" onClick={() => this.handleOk()} >
+                登录
+              </Button>
+            </Row>
+          </form>
+        </div>
+        <div className={styles.logo}>
+          <div>
+            <img alt="logo" src={config.logo} />
+            <p style={{ paddingTop: 20, fontSize: 16, color: 'white' }}>{config.name}</p>
+          </div>
+        </div>
       </div>
-      <form>
-        <FormItem hasFeedback>
-          {getFieldDecorator('username', {
-            rules: [
-              {
-                required: true,
-              },
-            ],
-          })(<Input autoComplete="username" onPressEnter={handleOk} placeholder="用户名" />)}
-        </FormItem>
-        <FormItem hasFeedback>
-          {getFieldDecorator('password', {
-            rules: [
-              {
-                required: true,
-              },
-            ],
-          })(<Input type="password" autoComplete="current-password" onPressEnter={handleOk} placeholder="密码" />)}
-        </FormItem>
-        <Row>
-          <Button type="primary" onClick={handleOk} loading={loading.effects.login}>
-            登录
-          </Button>
-        </Row>
-      </form>
-    </div>
-  )
+    )
+  }
 }
-
-Login.propTypes = {
+Index.propTypes = {
   form: PropTypes.object,
   dispatch: PropTypes.func,
   loading: PropTypes.object,
 }
+Index = Form.create({})(Index)
+export default connect((state) => { return ({ login: state.login }) })(Index)
 
-export default connect(({ loading }) => ({ loading }))(Form.create()(Login))
+// const Login = ({
+//   loading,
+//   dispatch,
+//   form: {
+//     getFieldDecorator,
+//     validateFieldsAndScroll,
+//   },
+// }) => {
+//   function handleOk () {
+//     validateFieldsAndScroll((errors, values) => {
+//       if (errors) {
+//         return
+//       }
+//       dispatch({ type: 'login/login', payload: values })
+//     })
+//   }
+
+//   return (
+//     <div className={styles.form}>
+//       <div className={styles.logo}>
+//         <img alt="logo" src={config.logo} />
+//         <span>{config.name}</span>
+//       </div>
+//       <form>
+//         <FormItem hasFeedback>
+//           {getFieldDecorator('username', {
+//             rules: [
+//               {
+//                 required: true,
+//               },
+//             ],
+//           })(<Input autoComplete="username" onPressEnter={handleOk} placeholder="用户名" />)}
+//         </FormItem>
+//         <FormItem hasFeedback>
+//           {getFieldDecorator('password', {
+//             rules: [
+//               {
+//                 required: true,
+//               },
+//             ],
+//           })(<Input type="password" autoComplete="current-password" onPressEnter={handleOk} placeholder="密码" />)}
+//         </FormItem>
+//         <Row>
+//           <Button type="primary" onClick={handleOk} loading={loading.effects.login}>
+//             登录
+//           </Button>
+//         </Row>
+//       </form>
+//     </div>
+//   )
+// }
+
+// Login.propTypes = {
+//   form: PropTypes.object,
+//   dispatch: PropTypes.func,
+//   loading: PropTypes.object,
+// }
+
+// export default connect(({ loading }) => ({ loading }))(Form.create()(Login))

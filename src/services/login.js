10dd57fdd387ec31b21d8665f48ca6e2580c6e05
pixcai <pixcai@163.com>
2019-01-17 15:15:53 +0800

    Add login count

--- src/services/login.js
+++ src/services/login.js
@@ -3,16 +3,45 @@ import config from 'config'
 import createHash from 'create-hash/browser'
 
 const { userLogin, userRegister } = config.api
+let retryCount = 0
+let totalSecond = 0
+let retryTimer = null
 
 export async function login (data) {
   const hash = createHash('sha256')
   hash.update(data.password)
   data.password = hash.digest('hex').toString()
 
+  if (totalSecond !== 0) {
+    return Promise.resolve({
+      data: {
+        success: false,
+        message: `请等待${totalSecond}秒后重试`,
+      },
+    })
+  }
+
   return request({
     url: userLogin,
     method: 'post',
     data,
+  }).then((res) => {
+    if (res && res.data && !res.data.success) {
+      retryCount += 1
+      totalSecond = retryCount * 3
+
+      retryTimer = setInterval(() => {
+        if (--totalSecond <= 0) {
+          clearInterval(retryTimer)
+        }
+      }, 1000)
+    } else {
+      retryCount = 0
+      totalSecond = 0
+      clearInterval(retryTimer)
+    }
+
+    return res
   })
 }
 
